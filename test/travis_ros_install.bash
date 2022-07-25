3801efafbe78f9ecb76a542f3cc7cc827ea56c98
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2022-07-25 11:59:00 +0000

    test_10

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -11,7 +11,57 @@ cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
-bash ./step1.bash
+#bash ./step1.bash
+#!/bin/bash -exv
+
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=melodic
+[ "$UBUNTU_VER" = "bionic" ] || exit 1
+
+echo "deb http://packages.ros.org/ros/ubuntu $UBUNTU_VER main" > /tmp/$$-deb
+sudo mv /tmp/$$-deb /etc/apt/sources.list.d/ros-latest.list
+
+set +vx
+while ! sudo apt-get install -y curl ; do
+	echo '***WAITING TO GET A LOCK FOR APT...***'
+	sleep 1
+done
+set -vx
+
+curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+sudo apt-get update || echo ""
+
+sudo apt-get install -y ros-${ROS_VER}-ros-base
+
+ls /etc/ros/rosdep/sources.list.d/20-default.list && sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
+sudo apt install -y python-pip
+sudo -H pip install rosdep
+sudo rosdep init 
+sudo rosdep update
+
+sudo apt-get install -y python-rosinstall
+sudo apt-get install -y build-essential
+
+grep -F "source /opt/ros/$ROS_VER/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$ROS_VER/setup.bash" >> ~/.bashrc
+
+grep -F "ROS_MASTER_URI" ~/.bashrc ||
+echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
+
+grep -F "ROS_HOSTNAME" ~/.bashrc ||
+echo "export ROS_HOSTNAME=localhost" >> ~/.bashrc
+
+sudo chown $USER:$USER $HOME/.ros/ -R
+
+### instruction for user ###
+set +xv
+
+echo '***INSTRUCTION*****************'
+echo '* do the following command    *'
+echo '* $ source ~/.bashrc          *'
+echo '* after that, try             *'
+echo '* $ LANG=C roscore            *'
+echo '*******************************'
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
