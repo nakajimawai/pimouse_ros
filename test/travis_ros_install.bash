1e68620bb0deeca9fdabf868230417c873706b64
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2022-07-25 12:22:26 +0000

    test_13_melodic

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -31,7 +31,7 @@ set -vx
 curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
 sudo apt-get update || echo ""
 
-sudo apt-get install -y ros-melodic-ros-base
+sudo apt-get install -y ros-${ROS_VER}-ros-base
 
 ls /etc/ros/rosdep/sources.list.d/20-default.list && sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
 sudo apt install -y python-pip
@@ -42,8 +42,8 @@ sudo rosdep update
 sudo apt-get install -y python-rosinstall
 sudo apt-get install -y build-essential
 
-grep -F "source /opt/ros/melodic/setup.bash" ~/.bashrc ||
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+grep -F "source /opt/ros/$ROS_VER/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$ROS_VER/setup.bash" >> ~/.bashrc
 
 grep -F "ROS_MASTER_URI" ~/.bashrc ||
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
@@ -62,7 +62,6 @@ echo '* $ source ~/.bashrc          *'
 echo '* after that, try             *'
 echo '* $ LANG=C roscore            *'
 echo '*******************************'
-
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
