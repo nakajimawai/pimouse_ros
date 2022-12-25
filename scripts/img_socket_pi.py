67e7818b4edd23b31b823ee74338e538cbe4f192
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2022-12-25 17:08:32 +0900

    カメラ映像をソケット通信でノートPCに送信

--- scripts/img_socket_pi.py
+++ scripts/img_socket_pi.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python
 import SocketServer
 import socket
+import cv2
+import numpy
 import sys
 
 class TCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-	print(self.request.recv(1024).strip().decode())
-	self.request.send(("hello world").encode("utf-8"))
+	self.data = self.request.recv(1024).strip()
+	ret, frame=capture.read()
+	jpegstring = cv2.imencode('.jpg', frame)[1].tostring()
+	self.request.send(jpegstring)
+
+'''HOST and PORT setting'''
 
 HOST = '192.168.143.152'
 PORT = 8080
+
+'''camera setting'''
+
+capture = cv2.VideoCapture(0)
+#capture.set(3,320)
+#capture.set(4,240)
+if not capture:
+    print("Could not open camera")
+    sys.exit()
+
+
 SocketServer.TCPServer.allow_reuse_address = True
 server = SocketServer.TCPServer((HOST, PORT), TCPHandler)
+server.capture = capture
 
 try:
     server.serve_forever()
