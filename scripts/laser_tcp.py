fc789af355be3726b23546ad3f0b0e350189e92d
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2023-05-24 11:49:29 +0900

    前回の障害物情報と変更があったときのみノートPCと通信を行うよう変更

--- scripts/laser_tcp.py
+++ scripts/laser_tcp.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 # _*_ coding: utf-8 _*_
 
-import socket, rospy
+import socket, rospy, time
 from std_msgs.msg import String
+
+str = 'first'
 
 '''sending by socket'''
 def send_laser_msg(msg):
+    s_time = time.time()
     HOST = '192.168.11.4'
     PORT = 50000
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
     client.sendall(msg)
+
+    e_time = time.time()
+    ex_time = e_time - s_time
+    print("ex_time: {:.10f}sec".format(ex_time))
+
     client.close()
 
 '''callback_function'''
 def callback(msg):
-    send_laser_msg(msg.data)
+    global str
+    if msg.data == str:
+	pass
+    else:
+	str = msg.data
+        send_laser_msg(msg.data)
 
 def listener():
     rospy.init_node('laser_tcp', anonymous=True)
