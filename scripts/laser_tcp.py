2495c1ff37f646b63cb65eedc8c4830b33fb31c1
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2023-05-30 17:30:16 +0900

    障害物情報を文字列の配列でノートPCに送信

--- scripts/laser_tcp.py
+++ scripts/laser_tcp.py
@@ -3,38 +3,45 @@
 
 import socket, rospy, time
 from std_msgs.msg import String
+from pimouse_ros.msg import StringArray
 
-str = 'first'
+str = ['first', 'first', 'first', 'first']
 
 '''sending by socket'''
 def send_laser_msg(msg):
-    s_time = time.time()
+    #print(msg)
+    #print(msg.data)
+    #print(iter(msg.data))
+    data = '|'.join(msg.data).encode()
+
+    
     HOST = '192.168.11.4'
     PORT = 50000
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
-    client.sendall(msg)
+    client.sendall(data)
 
-    e_time = time.time()
-    ex_time = e_time - s_time
-    print("ex_time: {:.10f}sec".format(ex_time))
 
     client.close()
 
 '''callback_function'''
 def callback(msg):
+    #rospy.loginfo('Received: %s', msg.data)
+
     global str
-    if msg.data == str:
-	pass
-    else:
-	str = msg.data
-        send_laser_msg(msg.data)
+    for i in range(4):
+        if msg.data[i] != str[i]:
+    	    str = msg.data
+	    send_laser_msg(msg)
+	    break
+        else:
+	    continue
 
 def listener():
     rospy.init_node('laser_tcp', anonymous=True)
-    rospy.Subscriber('laser_msg', String, callback)
+    rospy.Subscriber('laser_msg', StringArray, callback)
     rospy.spin()
 
 if __name__ == '__main__':
-    send_laser_msg('nothing')
+    #send_laser_msg(str)
     listener()
