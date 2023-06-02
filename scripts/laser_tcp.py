8b0f506981593fb34c788d18e6b5fd60c81c092d
Yuto Nakaijma <t19jm039@yamanashi.ac.jp>
2023-06-02 16:55:20 +0900

    ブール値の配列をいったん1つの文字列に変換してノートPCに送信

--- scripts/laser_tcp.py
+++ scripts/laser_tcp.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 # _*_ coding: utf-8 _*_
 
-import socket, rospy, time
+import socket, rospy, time, struct
 from std_msgs.msg import String
 from pimouse_ros.msg import StringArray
 
-str = ['first', 'first', 'first', 'first']
+str = [True, True, True, True]
 
 '''sending by socket'''
 def send_laser_msg(msg):
+    #data = '|'.join(msg.data).encode()
     #print(msg)
-    #print(msg.data)
-    #print(iter(msg.data))
-    data = '|'.join(msg.data).encode()
+    s_time = time.time()
 
-    
+    data = struct.pack('b'*len(msg.data), *(msg.data))
     HOST = '192.168.11.4'
     PORT = 50000
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
     client.sendall(data)
 
+    e_time = time.time()
+    ex_time = e_time - s_time
+    print("ex_time: {:.10f}sec".format(ex_time))
 
     client.close()
 
